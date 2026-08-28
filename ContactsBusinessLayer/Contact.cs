e51b220ed85831225c34a6409ce12310a922e97f
aymanhabbashprogramming <aymanhabbashprogramming@gmail.com>
2026-08-28 22:29:12 +0300

    Rename class to clsContact in Business layer

--- ContactsBusinessLayer/Contact.cs
+++ ContactsBusinessLayer/Contact.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ContactsBusinessLayer
 {   
-    public class Contact
+    public class clsContact
     {
+
     }
 }
