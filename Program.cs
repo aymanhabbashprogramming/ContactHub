ee7a05561dbe716fc1de1edec5037612b8b7629f
aymanhabbashprogramming <aymanhabbashprogramming@gmail.com>
2026-08-29 00:14:08 +0300

    Test Find contact functionality in PresentationLayer

--- Program.cs
+++ Program.cs
@@ -1,15 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ContactsBusinessLayer;
+using System;
+using System.Data;
+using System.Diagnostics.Contracts;
 
 namespace ContactsManagementSystemPresentationLayer
 {
     internal class Program
     {
+        public static void PrintInfo(clsContact contactInf)
+        {
+            Console.WriteLine(contactInf.FirstName + " " + contactInf.LastName);
+            Console.WriteLine(contactInf.Email);
+            Console.WriteLine(contactInf.Phone);
+            Console.WriteLine(contactInf.Address);
+            Console.WriteLine(contactInf.DateOfBirth);
+            Console.WriteLine(contactInf.CountryID);
+            Console.WriteLine(contactInf.ImagePath);
+        }
+        static void testFindContact(int ID)
+        {
+            clsContact contact = clsContact.Find(ID);
+
+            if (contact!= null)
+            {
+                PrintInfo(contact);
+            }
+            else
+            {
+                Console.WriteLine("Contact [" + ID + "] Not found!");
+            }
+
+        }
         static void Main(string[] args)
         {
+
+            testFindContact(7);
+            
         }
     }
 }
