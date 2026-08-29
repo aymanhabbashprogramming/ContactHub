e02a9f8ded06fe2fe5feac3dce7ac4f8713e6a6b
aymanhabbashprogramming <aymanhabbashprogramming@gmail.com>
2026-08-29 18:06:19 +0300

    Add Find method

--- ContactsBusinessLayer/Country.cs
+++ ContactsBusinessLayer/Country.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿using ContactsDataAccessLayer;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics.PerformanceData;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ContactsBusinessLayer
 {
-    internal class clsCountry
+    public class clsCountry
     {
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+
+        public int ID {  get; set; }
+        public string CountryName { get; set; }
+
+        clsCountry()
+        {
+            ID = -1;
+            CountryName = "";
+            this.Mode = enMode.AddNew;
+        }
+
+        clsCountry(int  ID,  string CountryName)
+        {
+            this.ID= ID;
+            this.CountryName= CountryName;
+            this.Mode= enMode.Update;
+        }
+
+
+        public static clsCountry Find(int ID)
+        {
+            string CountryName = "";
+            bool isFound = clsCountryData.GetCountryInfoByID(ID,ref CountryName);
+            if (isFound)
+            {
+                return new clsCountry(ID, CountryName);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
     }
 }
